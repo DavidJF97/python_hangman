d1de4617d35840fca1b6c694f91d3eb235b1d19b
David J-F <davidjf97@yahoo.ie>
2022-10-30 18:07:39 +0000

    Initial Commit

---
+++ words.py
@@ -0,0 +1,3 @@
+animals = ("chicken", "rabbit", "horse", "turtle", "dolphin", "puppy", "kitten", "meerkat", "giraffe")
+fruits = ("banana", "pineapple", "tomatoe", "cherries", "blueberry")
+movies = ("horror", "comedy", "romance", "classic", "action", "thriller", "western")
