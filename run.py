24d5fa3994886f2167fb0742472741fb6d32573c
David J-F <davidjf97@yahoo.ie>
2022-10-31 15:01:12 +0000

    Outline code for menu and category choice and update requirements.txt file

--- run.py
+++ run.py
@@ -2,14 +2,52 @@ import random
 from colorama import init, Fore, Back
 from game_graphics import header, win, lose
 from words import animals, fruits, movies
+import sys
+import operator
+import os
 
-
+def clear_screen():
+    os.system("cls")
 
 def get_word(p_choice):
     word = random.choice(p_choice)
     return word.upper()
 
 # if else statement for players choice of category here
+
+def game_menu():
+    """
+    Game menu, player can chose to start game, read the rules or exit
+    """
+    print(header)
+    print ("Press " + Fore.RED + "1" + Fore.WHITE +
+           " to start the game")
+    print ("Press " + Fore.RED + "2" + Fore.WHITE +
+           " to read the rules")
+    print("Press " + Fore.RED + "3" + Fore.WHITE +
+          " to exit game")
+    while True:
+        user_choice = input("\n")
+        if user_choice == "1":
+            choose_category()
+        elif user_choice == "2":
+            show_rules()
+        elif user_choice == "3":
+             sys.exit()
+        else:
+            print("Must choose 1, 2 or 3 !")
+
+
+def choose_category():
+    """
+    Player can chose category of words to guess from.
+    """
+    clear_screen()
+    print("Select a category:")
+    print("Press 1: " + Fore.RED + "Animals")
+    print(Fore.WHITE + "Press 2: " + Fore.RED + "Fruits")
+    print(Fore.WHITE + "Press 3: " + Fore.RED + "Movie Genres")
+
 which = get_word(animals)
 
 print(which)
@@ -54,16 +92,16 @@ def play(word):
                 guessed = True
                 guess_word = word
         else:
-        print("Not a valid guess")
-        print(display_hangman(lives))
-        print(guess_word)
-        print("\n")
+            print("Not a valid guess")
+            print(display_hangman(lives))
+            print(guess_word)
+            print("\n")
     if guessed:
         print("Congratulations! You got the word.\n")
         print(win)
-    else
-    print("Sorry. You're out of lives.\n")
-    print(lose)
+    else:
+        print("Sorry. You're out of lives.\n")
+        print(lose)
 
 
 
@@ -144,3 +182,8 @@ def hangman_lives(lives):
         """
     ]
     return lives_left[lives]
+
+def main():
+    game_menu()
+
+main()
